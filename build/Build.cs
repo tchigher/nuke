a9218033ca0583d19e99ec09bbe299a19ee89dd3
Matthias Koch <ithrowexceptions@gmail.com>
2020-10-28 12:25:51 +0100

    WIP

--- build/Build.cs
+++ build/Build.cs
@@ -93,7 +93,7 @@ partial class Build : NukeBuild
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-    public static int Main() => Execute<Build>(x => x.Pack);
+    public static int Main() => Execute<Build>(x => x.Analysis);
 
     [CI] readonly TeamCity TeamCity;
     [CI] readonly AzurePipelines AzurePipelines;
